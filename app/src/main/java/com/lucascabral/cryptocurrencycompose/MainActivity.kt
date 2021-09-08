2ae8ad361ed57df7684e600d0fff2d79d05dbc07
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-09-08 08:35:43 -0300

    Init commit

---
+++ app/src/main/java/com/lucascabral/cryptocurrencycompose/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.lucascabral.cryptocurrencycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.lucascabral.cryptocurrencycompose.ui.theme.CryptocurrencyComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CryptocurrencyComposeTheme {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CryptocurrencyComposeTheme {
+
+    }
+}
